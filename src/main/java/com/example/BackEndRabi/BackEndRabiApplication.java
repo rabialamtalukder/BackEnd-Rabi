1ec2724627c2dd624731830f0aa137e9d112e67c
rabi alam <rabi_004@hotmail.com>
2020-04-08 02:39:33 -0500

    initial commit

---
+++ src/main/java/com/example/BackEndRabi/BackEndRabiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndRabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndRabiApplication.class, args);
+	}
+
+}
